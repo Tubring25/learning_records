9b01fd6d31b602b604826b2c7484f17e136d33da
yuqianqian <yuqianqian@senses-ai.com>
2022-04-17 18:32:16 +0800

    delete unuseful files

--- TypeScript/basic info/src/index.ts
+++
@@ -1,4 +0,0 @@
-import './style/index.less'
-import GameControl from './GameControl'
-
-let gameControl = new GameControl()
--- TypeScript/基础/1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var str = 'hello';
-var unkonw; // unknow类型可以是任意  不能直接赋值给其他变量
-var any; // any类型可以是任意值  能直接赋值给其他变量，并且将其他变量变为any类型
-// 类型断言
-var s = str;
-s = str;
